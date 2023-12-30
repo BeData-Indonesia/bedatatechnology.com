01dc4059c2d058f10a941b6ed9e5c24b155913d4
fairus <fairusramadhani29@gmail.com>
2023-12-30 10:24:29 +0700

    init project

---
+++ resources/js/app.tsx
@@ -0,0 +1,18 @@
+require("./bootstrap");
+
+import { render } from "react-dom";
+import { createInertiaApp } from "@inertiajs/inertia-react";
+import { InertiaProgress } from "@inertiajs/progress";
+import * as React from "react";
+
+const appName = "Bedata";
+//const appName = window.document.getElementsByTagName("title")[0]?.innerText
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        return render(<App {...props} />, el);
+    },
+});
+
+InertiaProgress.init({ color: "#4B5563" });
