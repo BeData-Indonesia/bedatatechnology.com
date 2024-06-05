8f3392475cfcb226bc21603e6729b988a05717a0
melanierefman <87106838+melanierefman@users.noreply.github.com>
2024-06-05 19:25:20 +0700

    fixing : delete comment

--- resources/js/pages/admin/articles/category/index.tsx
+++ resources/js/pages/admin/articles/category/index.tsx
@@ -51,7 +51,6 @@ const CategoryIndex: React.FC<Props> = ({ categories }) => {
             <table className="table-auto w-full">
                 <thead>
                     <tr>
-                        {/* <th className="px-4 py-2">ID</th> */}
                         <th className="px-4 py-2">Name</th>
                         <th className="px-4 py-2">Actions</th>
                     </tr>
@@ -59,7 +58,6 @@ const CategoryIndex: React.FC<Props> = ({ categories }) => {
                 <tbody>
                     {categories.map((category) => (
                         <tr key={category.id}>
-                            {/* <td className="border px-4 py-2">{category.id}</td> */}
                             <td className="border px-4 py-2">
                                 {category.name}
                             </td>
